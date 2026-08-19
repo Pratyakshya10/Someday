091cdd232c11c8e2ee4781e14d3eeb0a6d6e1cd7
Pratyakshya Mishra <pratyakshya.mishra43@gmail.com>
2026-08-19 22:29:29 +0530

    add barrel export

---
+++ src/landing/index.ts
@@ -0,0 +1,9 @@
+export { Landing } from "./Landing";
+
+export { Nav } from "./sections/Nav";
+export { Hero } from "./sections/Hero";
+export { WhatItIs } from "./sections/WhatItIs";
+export { Examples } from "./sections/Examples";
+export { Together } from "./sections/Together";
+export { FinalCta } from "./sections/FinalCta";
+export { Footer } from "./sections/Footer";
